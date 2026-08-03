7d641cb6a5cb0cb499207df27caa458c07ddb21d
kheguy <g7tro4w@gmail.com>
2026-08-03 11:04:33 -0700

    Another protocol fix for agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -57,7 +57,7 @@ func (a *Agent) Start(pollInterval time.Duration, reportInterval time.Duration) 
 						mValue = strconv.FormatFloat(value.(float64), 'f', -1, 64)
 					}
 
-					_, err := a.httpClient.Post(fmt.Sprintf("http://%s/update/%s/%s/%s", a.url, mType, name, mValue), "plain/text", strings.NewReader(""))
+					_, err := a.httpClient.Post(fmt.Sprintf("%s/update/%s/%s/%s", a.url, mType, name, mValue), "plain/text", strings.NewReader(""))
 
 					if err != nil {
 						fmt.Printf("Request error: \n%s\n", err.Error())
@@ -153,7 +153,9 @@ func main() {
 
 	storage := repository.MakeNewMemoryStorage()
 
-	agent := NewAgent(storage, *address, *http.DefaultClient)
+	addressWithProtocol := "http://" + *address
+
+	agent := NewAgent(storage, addressWithProtocol, *http.DefaultClient)
 
 	agent.Start(time.Duration(*pollInterval)*time.Second, time.Duration(*reportInterval)*time.Second)
 
