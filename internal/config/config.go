ca322f226d704b0203b22ace25e69623ccb88a6a
kheguy <g7tro4w@gmail.com>
2026-09-17 09:49:06 -0700

    Fixed default bd address

--- internal/config/config.go
+++ internal/config/config.go
@@ -49,7 +49,7 @@ func LoadServer() (ServerConfig, error) {
 		StoreInterval:   300,
 		FileStoragePath: "/tmp/collector-metrics.json",
 		Restore:         true,
-		DBAddress:       "postgres://postgres@localhost:5432/postgres?sslmode=disable",
+		DBAddress:       "",
 	}
 
 	flags := flag.NewFlagSet("server", flag.ContinueOnError)
